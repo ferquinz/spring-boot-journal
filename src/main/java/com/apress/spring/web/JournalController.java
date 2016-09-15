bdf322b310051f1b3b8420b5d2ebecc4e3090e30
fsanchiz <fernando.sanchiz@momit.com>
2016-09-15 16:55:03 +0200

    First test method

--- src/main/java/com/apress/spring/web/JournalController.java
+++ src/main/java/com/apress/spring/web/JournalController.java
@@ -1,18 +1,33 @@
 package com.apress.spring.web;
 
 import com.apress.spring.domain.Journal;
+import com.apress.spring.domain.JournalEntry;
 import com.apress.spring.repository.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Controller
+@RestController
 public class JournalController {
+
+    private static List<JournalEntry> entries = new ArrayList<>();
+    static {
+        try{
+            entries.add(new JournalEntry("Get to know Spring Boot", "Today I will learn Spring Boot", "01/01/2016"));
+            entries.add(new JournalEntry("Simple Spring Boot Project", "I will do my first Spring Boot Project", "01/02/2016"));
+            entries.add(new JournalEntry("Spring Boot Reading", "Read more about Spring Boot", "02/01/2016"));
+            entries.add(new JournalEntry("Spring Boot in the Cloud", "Spring Boot using Cloud Foundry", "03/01/2016"));
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
 
     @Autowired
     JournalRepository repo;
@@ -27,4 +42,21 @@ public class JournalController {
     public @ResponseBody List<Journal> getJournal(){
         return repo.findAll();
     }
+
+    @RequestMapping(value = "/journal/all")
+    public @ResponseBody List<JournalEntry> getAllJournal() throws ParseException{
+        return entries;
+    }
+
+    @RequestMapping(value = "/journal/findBy/title/{title}")
+    public List<JournalEntry> findByTitleContains(@PathVariable String title) throws ParseException{
+        return entries.stream().filter(journalEntry -> journalEntry.getTitle().toLowerCase().contains(title.toLowerCase())).collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/journal", method = RequestMethod.POST)
+    public JournalEntry add(@RequestBody JournalEntry entry){
+        entries.add(entry);
+        return entry;
+    }
+
 }
--- src/test/java/com/apress/spring/SpringBootJournalApplicationTests.java
+++ src/test/java/com/apress/spring/SpringBootJournalApplicationTests.java
@@ -1,13 +1,45 @@
 package com.apress.spring;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.Matchers.iterableWithSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class SpringBootJournalApplicationTests {
+
+	private MediaType mediaType = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception{
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void getAll() throws Exception {
+		mockMvc.perform(get("/journal/all")).andExpect(status().isOk()).andExpect(content().contentType(mediaType)).andExpect(jsonPath("$", iterableWithSize(4)));
+	}
 
 	@Test
 	public void contextLoads() {
