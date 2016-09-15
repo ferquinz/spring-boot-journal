49ed1af551a88fe507774a3fe315ab0b9edc1665
fsanchiz <fernando.sanchiz@momit.com>
2016-09-15 17:17:21 +0200

    Finished tests

--- src/test/java/com/apress/spring/SpringBootJournalApplicationTests.java
+++ src/test/java/com/apress/spring/SpringBootJournalApplicationTests.java
@@ -1,19 +1,27 @@
 package com.apress.spring;
 
+import com.apress.spring.domain.JournalEntry;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.iterableWithSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,11 +33,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class SpringBootJournalApplicationTests {
 
+    @SuppressWarnings("rawtypes")
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
 	private MediaType mediaType = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 	private MockMvc mockMvc;
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters){
+        mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(converter -> converter instanceof MappingJackson2HttpMessageConverter).findAny().get();
+    }
 
 	@Before
 	public void setup() throws Exception{
@@ -38,8 +53,36 @@ public class SpringBootJournalApplicationTests {
 
 	@Test
 	public void getAll() throws Exception {
-		mockMvc.perform(get("/journal/all")).andExpect(status().isOk()).andExpect(content().contentType(mediaType)).andExpect(jsonPath("$", iterableWithSize(4)));
+		mockMvc.perform(get("/journal/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$", iterableWithSize(4)))
+                .andExpect(jsonPath("$[0]['title']", containsString("Spring Boot")));
 	}
+
+    @Test
+    public void findByTitle() throws Exception {
+        mockMvc.perform(get("/journal/findBy/title/" + "Cloud"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(mediaType))
+                .andExpect(jsonPath("$", iterableWithSize(1)))
+                .andExpect(jsonPath("$[0]['title']", containsString("Cloud")));
+    }
+
+    @Test
+    public void add() throws Exception {
+        mockMvc.perform(post("/journal")
+                .content(this.toJsonString(new JournalEntry("Spring Boot Testing", "Create Spring Boot Tests", "05/09/2016")))
+                .contentType(mediaType))
+                .andExpect(status().isOk());
+    }
+
+    @SuppressWarnings("unchecked")
+    protected String toJsonString(Object obj) throws IOException{
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        mappingJackson2HttpMessageConverter.write(obj, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
 
 	@Test
 	public void contextLoads() {
